38bd0d67d62dcf50f38662964588b84c961dec41
mani <111manikanta.v@gmail.com>
2023-08-04 19:04:01 +0530

    notes explanation of interfaces

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,6 +1,15 @@
 "use strict";
 class Person {
     constructor(name, age) {
+        this.gender = {
+            gender: "male/female/other",
+            orientation: "//straight/etc...",
+            pronouns: "he/she/him..."
+        };
+        this.address = "2-73";
+        this.email = "111ma...";
+        this.phone = [];
+        this.git = "MANI-116";
         this.name = name;
         this.age = age;
     }
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,11 +1,44 @@
-interface PersonInterface {
-    name:string;
-    age:number
+//interfaces uses Pascal naming convention
+//interfaces can accumulate data of specific type
+//interfaces can use other interfaces
+//interfaces can extend interfaces
+//interfaces can be implemented by classes
+
+interface PersonGenderInterface {
+    gender:string; //male/female/other
+    orientation:string; //straight/etc...
+    pronouns:string; //he,she,Mr.,Mrs.,
 }
 
-class Person implements PersonInterface{
-    name:string;
+interface PersonContactDetails{
+    address:string;
+    email:string;
+    phone:number[];
+    git:string;
+    
+}
+
+interface PersonInterface extends PersonContactDetails{ //interface extends interfaces
+    name:string; //interfaces let you accumulate data of a specific type
     age:number;
+    gender:PersonGenderInterface;   //interfaces uses interfaces
+}
+
+
+
+class Person implements PersonInterface{   //classes can implements interfaces
+    name:string; 
+    age:number;
+    gender:PersonGenderInterface 
+    = {
+        gender : "male/female/other",
+        orientation:"//straight/etc...",
+        pronouns:"he/she/him..."
+        } 
+    address:string="2-73";
+    email:string="111ma...";
+    phone:number[]=[];
+    git:string="MANI-116";
 
     constructor(name:string,age:number){
         this.name = name;
