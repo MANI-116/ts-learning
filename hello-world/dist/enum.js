bd816527ffcdbacfbb0b7826ffd3a823d6168bdb
mani <111manikanta.v@gmail.com>
2023-08-07 03:55:20 +0530

    enums pratice

---
+++ hello-world/dist/enum.js
@@ -0,0 +1,13 @@
+"use strict";
+var Arithmetic;
+(function (Arithmetic) {
+    Arithmetic[Arithmetic["Add"] = 0] = "Add";
+    Arithmetic[Arithmetic["Mul"] = 1] = "Mul";
+    Arithmetic[Arithmetic["Div"] = 2] = "Div";
+    Arithmetic[Arithmetic["Sub"] = 3] = "Sub";
+})(Arithmetic || (Arithmetic = {}));
+function calculate(a, b, type) {
+    console.log("calulate " + type + " of " + a + " and " + b);
+}
+let ans = calculate(1, 3, Arithmetic.Mul);
+//# sourceMappingURL=enum.js.map
